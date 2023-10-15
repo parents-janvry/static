2f8441f55ec798f2fc7f6a2037c68c8bf396acd5
Simon Garnotel <simon.garnotel@gmail.com>
2023-10-15 21:55:49 +0200

    update

---
+++ email/emails/parents-janvry.tsx
@@ -0,0 +1,148 @@
+import React from 'react'
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Row,
+  Section,
+  Text,
+} from '@react-email/components'
+
+import style from '../style'
+
+const columns: React.CSSProperties = {
+  marginTop: '15px',
+}
+
+const column: React.CSSProperties = {
+  width: '30%',
+}
+
+const columnImg: React.CSSProperties = {
+  maxWidth: '80%',
+  maxHeight: '160px',
+}
+
+const columnText: React.CSSProperties = {
+  color: '#677876',
+  margin: '5px 0',
+}
+
+const columnTextLatest: React.CSSProperties = {
+  color: '#677876',
+  margin: '0',
+  marginBottom: '15px',
+}
+
+const Presentation = () => {
+  return (
+    <Html>
+      <Head />
+      <Body style={style.body}>
+        <Container style={style.container}>
+          <Section style={style.section}>
+            <Row>
+              <Column align='center'>
+                <Img
+                  src='https://parents-janvry.github.io/static/header.png'
+                  style={style.headerImg}
+                  alt='entête'
+                />
+              </Column>
+            </Row>
+            <Text style={style.paragraph}>Chers Parents,</Text>
+            <Text style={style.paragraph}>
+              Cette semaine, vous avez été conviés à élire l'équipe des parents
+              délégués qui vous accompagneront durant cette année scolaire. Le
+              dépouillement à eu lieu ce vendredi et vous nous avez choisi (Le
+              choix était difficile 😀) pour vous représenter.
+            </Text>
+            <Text style={style.paragraph}>
+              Voici une petite présentation de l'équipe pour que vous puissiez
+              nous identifier facilement. N'hésitez surtout pas à nous
+              solliciter par{' '}
+              <Link style={style.link} href='mailto:parents.janvry@gmail.com'>
+                email
+              </Link>{' '}
+              ou directement devant le portail de l'école.
+            </Text>
+            <Section style={columns}>
+              <Row>
+                <Column style={column} align='center'>
+                  <Img
+                    src='https://parents-janvry.github.io/static/narimane.png'
+                    alt='narimane'
+                    style={columnImg}
+                  />
+                  <Text style={columnText}>Narimane DJEBIRET</Text>
+                  <Text style={columnTextLatest}>Lila (CE2), Largo (PS)</Text>
+                </Column>
+                <Column style={column} align='center'>
+                  <Img
+                    src='https://parents-janvry.github.io/static/magali.png'
+                    alt='magali'
+                    style={columnImg}
+                  />
+                  <Text style={columnText}>Magali BEAUMONT</Text>
+                  <Text style={columnTextLatest}>Enola (CE1), Maeli (CP)</Text>
+                </Column>
+                <Column style={column} align='center'>
+                  <Img
+                    src='https://parents-janvry.github.io/static/esma.png'
+                    alt='esma'
+                    style={columnImg}
+                  />
+                  <Text style={columnText}>Esma BENTCHIKOU</Text>
+                  <Text style={columnTextLatest}>Elea (CM2), Fanny (CP)</Text>
+                </Column>
+              </Row>
+              <Row>
+                <Column style={column} align='center'>
+                  <Img
+                    src='https://parents-janvry.github.io/static/stephanie.png'
+                    alt='stephanie'
+                    style={columnImg}
+                  />
+                  <Text style={columnText}>Stéphanie ZOPPELLO</Text>
+                  <Text style={columnTextLatest}>Stella (CE2)</Text>
+                </Column>
+                <Column style={column} align='center'>
+                  <Img
+                    src='https://parents-janvry.github.io/static/audrey.png'
+                    alt='audrey'
+                    style={columnImg}
+                  />
+                  <Text style={columnText}>Audrey GERAULT</Text>
+                  <Text style={columnTextLatest}>Simon (GS)</Text>
+                </Column>
+                <Column style={column} align='center'>
+                  <Img
+                    src='https://parents-janvry.github.io/static/celine.png'
+                    alt='celine'
+                    style={columnImg}
+                  />
+                  <Text style={columnText}>Céline HAAS</Text>
+                  <Text style={columnTextLatest}>Livia (CE1), Enzo (MS)</Text>
+                </Column>
+              </Row>
+            </Section>
+            <Text style={style.paragraph}>
+              Vous recevrez lorsqu'il sera nécessaire une "Lettre d'information"
+              pour vous communiquer les événements, activités ou informations
+              qui auront lieu tout au long de l'année.
+            </Text>
+            <Text style={style.paragraph}>Passez une excellente semaine!</Text>
+            <Text style={style.paragraph}>Les parents délégués.</Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  )
+}
+
+export default Presentation
---
+++ email/style.tsx
@@ -0,0 +1,68 @@
+const style: { [key: string]: React.CSSProperties } = {
+  body: {
+    backgroundColor: '#f5f5f5',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '16px',
+  },
+  container: {
+    backgroundColor: '#ffffff',
+    margin: '0 auto',
+    padding: '20px 0 48px',
+    marginBottom: '64px',
+  },
+  link: {
+    color: '#1d8348',
+    textDecoration: 'none',
+  },
+  section: {
+    padding: '0 20px',
+  },
+  headerImg: {
+    maxWidth: '90%',
+  },
+  title: {
+    color: '#1d8348',
+    textAlign: 'center',
+    margin: 0,
+  },
+  line: {
+    border: '2px dashed #1d8348',
+    width: 'auto',
+    margin: '20px 60px',
+  },
+  paragraph: {
+    textAlign: 'justify',
+  },
+  //   description {
+  //     display: flex;
+  //     flex-direction: column;
+  //   }
+  //   .photos {
+  //     display: grid;
+  //     grid-template-columns: 1fr 1fr 1fr;
+  //     grid-template-rows: 1fr 1fr;
+  //     margin-top: 15px;
+  //   }
+  //   .description > div {
+  //     text-align: center;
+  //     padding-top: 5px;
+  //     color: #677876;
+  //   }
+  //   .description > div:last-child {
+  //     padding-bottom: 15px;
+  //   }
+  //   .photo {
+  //     max-height: 150px;
+  //     margin: auto;
+  //   }
+  //   @media (max-width: 600px) {
+  //     body {
+  //       padding: 10px;
+  //     }
+  //     .photos {
+  //       grid-template-columns: 1fr 1fr;
+  //     }
+  //   }
+}
+
+export default style
